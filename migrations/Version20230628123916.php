575cdf588cc6a3f45032ee131564ba0f2f88c232
achref <achref.chouikh@gmail.com>
2023-06-28 16:15:20 +0200

    oklm

--- migrations/Version20230628123916.php
+++ migrations/Version20230628123916.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230621161500 extends AbstractMigration
+final class Version20230628123916 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -29,7 +29,7 @@ final class Version20230621161500 extends AbstractMigration
         $this->addSql('CREATE TABLE voiture (id INT AUTO_INCREMENT NOT NULL, image_name VARCHAR(255) DEFAULT NULL, prix DOUBLE PRECISION NOT NULL, date_circulation VARCHAR(255) NOT NULL, kilometrage INT NOT NULL, caracteristiques VARCHAR(255) DEFAULT NULL, equipement VARCHAR(255) DEFAULT NULL, options VARCHAR(255) DEFAULT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE contact ADD CONSTRAINT FK_4C62E638181A8BA FOREIGN KEY (voiture_id) REFERENCES voiture (id)');
-        $this->addSql('ALTER TABLE services_user ADD CONSTRAINT FK_9AA8EF8AEF5A6C1 FOREIGN KEY (services_id) REFERENCES services (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE services_user ADD CONSTRAINT FK_9AA8EF8AEF5A6C1 FOREIGN KEY (serviceoccs_id) REFERENCES services (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE services_user ADD CONSTRAINT FK_9AA8EF8A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
 
