374908b09e323cb36ec98c3e1a111100138f7f95
achref <achref.chouikh@gmail.com>
2023-06-28 10:39:12 +0200

    dash

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -14,13 +14,20 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Horaire;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 class DashboardController extends AbstractDashboardController
 {
+    private $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render ('admin/dash.html.twig');
-       
+        return $this->render('admin/dash.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -28,22 +35,19 @@ class DashboardController extends AbstractDashboardController
         return Dashboard::new()
             ->setTitle('V.Parrot Administration');
     }
-    
-    
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Employes', 'fas fa-user', User::class);
+
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::linkToCrud('Employes', 'fas fa-user', User::class);
+        }
+
         yield MenuItem::linkToCrud('Services', 'fas fa-screwdriver-wrench', Services::class);
         yield MenuItem::linkToCrud('Horaires', 'fas fa-stopwatch', Horaire::class);
         yield MenuItem::linkToCrud('Messages', 'fas fa-message', Contact::class);
         yield MenuItem::linkToCrud('Voitures', 'fas fa-car', Voiture::class);
         yield MenuItem::linkToCrud('Temoignages', 'fas fa-pen-nib', Temoignage::class);
-
-
-
-
-        
     }
 }
