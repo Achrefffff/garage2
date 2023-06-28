820d2f1033401047c121b6b9de7e6f80b19c20d7
achref <achref.chouikh@gmail.com>
2023-06-28 16:34:02 +0200

    oklm

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -29,6 +29,7 @@ class HomeController extends AbstractController
         
 
         $services = $servicesRepository->findAll();
+        $horaires = $horaireRepository->findAll();
         
 
        
